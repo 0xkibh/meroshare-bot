0f4e57233015352cd412c8e3b6355c2a31793926
tprototype <kishmat17@gmail.com>
2023-01-18 20:31:24 +0545

    Updated code and Added readme

---
+++ README.md
@@ -0,0 +1,48 @@
+# MEROSHARE BOT
+
+This is meroshare bot, which will auto apply IPO of different company which are open.
+This is bet
+
+## Get Started
+
+To get started, you need to download this project or can clone this repo on your machine.
+And you can simply run the main.py file. But before that you need to setup the environment.
+
+### 1. Install the dependencies
+
+```
+pip install -r requirments.txt
+# or
+python3 -m pip install -r requirements.txt
+```
+
+### 2. Run following code in the terminal
+
+```
+playwright install chromium
+```
+
+### 3. Edit demats.txt file to setup your information in the serial order as given
+
+```
+Name,CapitalID,Username,Password,Bank Name,Kitta,CRN,TXN_PIN
+
+For example:
+Ram,13700,0124525,password123,NIC ASIA BANK LTD.,10,JYU8281811,1234
+
+Bank Name can be : "NIC ASIA BANK LTD.", "NABIL BANK LTD." (i.e Official Name)
+
+"Ram" here is just a placeholder to denote the account, it can be anything you like
+```
+
+### 4. Now just run main.py file to run the script
+
+```
+python main.py
+# or
+python3 main.py
+```
+
+### You can raise issues or send message to ask if there is any problem you faced in any step
+### And suggestion are welcome :)
+
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import time
 from playwright.sync_api import sync_playwright
-from data import *
 import neotermcolor
 from tabulate import tabulate
 
@@ -19,29 +18,34 @@ def login(db_id,username,password):
     time.sleep(1)
 
     page.query_selector(".sign-in").click()
+    time.sleep(3)
+    
 
 def IPODict(list_):
     ipoData = [i.inner_text().split('\n') for i in list_]
-    neotermcolor.cprint(ipoData)
 
     IPOs = [[ipoData.index(ipo) + 1,ipo[0], f'{ipo[4]} {ipo[3]}'] for ipo in ipoData]
 
     return IPOs
 
 def gotoAsba():
-    page.goto("https://meroshare.cdsc.com.np/#/asba")
+    
+    page.query_selector('xpath=//*[@id="sideBar"]/nav/ul/li[8]/a').click()
+    
     time.sleep(5)
+    print(page.url)
     share = page.query_selector_all(".company-name")
     columns = ["Options","Company","Type"]
     data = IPODict(share)
     neotermcolor.cprint(tabulate(data,headers=columns,tablefmt="grid"),"green")
+    return len(data)
 
-def ipo_selector(ind=''):
-    if (ind == 0):
+def ipo_selector(total,ind=''):
+    if (total == 1):
         iposelector_index = ''
     else:
         iposelector_index = '[{}]'.format(ind)
-
+    
     xpath_string = '//*[@id="main"]/div/app-asba/div/div[2]/app-applicable-issue/div/div/div/div/div' + iposelector_index +'/div/div[2]/div/div[4]/button'
     apply_btn = page.query_selector(f"xpath ={xpath_string}")
     time.sleep(2)
@@ -55,45 +59,66 @@ def apply_share(bank,kitta,crn,txn_no):
     page.query_selector("#appliedKitta").click()
     page.query_selector("#appliedKitta").type(kitta)
     page.query_selector("#crnNumber").fill(crn)
-    time.sleep(2)
+    time.sleep(1)
     page.query_selector("#disclaimer").click()
-    time.sleep(5)
+    time.sleep(3)
     # print(page.query_selector("button[type=submit]").inner_text())
     page.get_by_role("button",name="Proceed").click()
-    print("Btn Clicked")
 
     page.query_selector("#transactionPIN").type(txn_no)
-    # change the .inner_text() to .click() to apply
-    print(page.get_by_role("button",name="Apply").inner_text())
+
+    page.get_by_role("button",name="Apply").click()
     neotermcolor.cprint("IPO Applied Successfully","green")
+
+    # below code to logout the current user
+    page.query_selector(".header-menu__link").click()
 
 
 with sync_playwright() as p:
+    # Description Banner for the tool
+    print("""
+    -----------------------------------------------------------------------------------------
+    ███╗   ███╗███████╗██████╗  ██████╗ ███████╗██╗  ██╗ █████╗ ██████╗ ███████╗    ██████╗  ██████╗ ████████╗
+    ████╗ ████║██╔════╝██╔══██╗██╔═══██╗██╔════╝██║  ██║██╔══██╗██╔══██╗██╔════╝    ██╔══██╗██╔═══██╗╚══██╔══╝
+    ██╔████╔██║█████╗  ██████╔╝██║   ██║███████╗███████║███████║██████╔╝█████╗      ██████╔╝██║   ██║   ██║   
+    ██║╚██╔╝██║██╔══╝  ██╔══██╗██║   ██║╚════██║██╔══██║██╔══██║██╔══██╗██╔══╝      ██╔══██╗██║   ██║   ██║   
+    ██║ ╚═╝ ██║███████╗██║  ██║╚██████╔╝███████║██║  ██║██║  ██║██║  ██║███████╗    ██████╔╝╚██████╔╝   ██║   
+    ╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝    ╚═════╝  ╚═════╝    ╚═╝   
+                                                                                                            
+    -----------------------------------------------------------------------------------------
+    Author: Kishmat Bhattarai
+    Github repository: https://github.com/tprototype/meroshare-bot
+    Twitter: @tprototypex
+    -----------------------------------------------------------------------------------------
+        """)
     browser = p.chromium.launch(headless=False)
     page = browser.new_page(user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36')
     page.goto("https://meroshare.cdsc.com.np")
     time.sleep(5)
-    # page.goto("https://google.com")
-    # page.query_selector_all(".company-name")
-    # page.query_selector("").inner_text()
+
     total = []
     with open("demats.txt", 'r') as f:
         for x in f.readlines():
             x = x[0:-1]
             total.append(x.split(","))
-    print(total)
+
 
     for account in total:
         name, dp_id, username, password, bank, kitta, crn, txn_pin = account
-        print('Working for ' + name)
+        print('Working for ' + name + ' :')
         login(dp_id,username,password)
         time.sleep(2)
-        gotoAsba()
-        time.sleep(2)
-        position = int(input("Enter the option number you want to apply IPO for : "))
-        ipo_selector(position-1)
+        try:
+            total = gotoAsba()
+        except:
+            neotermcolor.cprint(f"Login Error, recheck credentials of {name}","red")
+            neotermcolor.cprint("-------------------------------------------","red")
+            continue
+        position = int(input("Enter the OPTION NUMBER you want to apply IPO for : "))
+        ipo_selector(total,position)
         time.sleep(2)
         apply_share(bank,kitta,crn,txn_pin)
         time.sleep(5)
+        neotermcolor.cprint("-------------------------------------------","green")
 
     browser.close()
